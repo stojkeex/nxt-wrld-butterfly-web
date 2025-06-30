70bfd8db218e0e5af61c040885c2c2332d15449c
stojkeex <stackflow.info@gmail.com>
2025-06-30 19:39:35 +0200

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,16 @@
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './Home';
 
 const Index = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://embed.tawk.to/6862cb320a9288190d1bbe2c/1iv0ujgl2';
+    script.async = true;
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
+    document.body.appendChild(script);
+  }, []);
+
   return <Home />;
 };
 
