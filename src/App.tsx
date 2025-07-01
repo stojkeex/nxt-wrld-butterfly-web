da0b73d6aac884c6123636f009908151a05c67ff
stojkeex <stackflow.info@gmail.com>
2025-07-01 14:15:11 +0200

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -36,14 +36,6 @@ const App = () => {
     };
   }, [isSupportOpen]);
 
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://embed.tawk.to/6862cb320a9288190d1bbe2c/1iv0ujgl2';
-    script.async = true;
-    script.charset = 'UTF-8';
-    script.setAttribute('crossorigin', '*');
-    document.body.appendChild(script);
-  }, []);
 
   useEffect(() => {
     const fetchMessage = async () => {
